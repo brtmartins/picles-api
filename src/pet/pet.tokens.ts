b27c38d9e0a193e9226cc68a022ff5a7088a7b63
brtmartins <brunathauanemartins@gmail.com>
2024-04-08 22:36:22 -0300

    Criação do input, output, controller e schema  do Pet

---
+++ src/pet/pet.tokens.ts
@@ -0,0 +1,6 @@
+export default class PetTokens {
+  static CreatePetUseCase(CreatePetUseCase: any): (target: import("src/pet/pet.controller").PetController, propertyKey: "createPetUseCase") => void {
+    throw new Error('Method not implemented.');
+  }
+  static createPetUseCase = 'createPetUseCase';
+}
