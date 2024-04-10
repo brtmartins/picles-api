802407487af195bb926dcca694260e96083bd932
brtmartins <brunathauanemartins@gmail.com>
2024-04-09 22:30:59 -0300

    Criação da rotina de get pet by id e mensagens personalizadas de erro.

--- src/pet/pet.tokens.ts
+++ src/pet/pet.tokens.ts
@@ -1,6 +1,5 @@
 export default class PetTokens {
-  static CreatePetUseCase(CreatePetUseCase: any): (target: import("src/pet/pet.controller").PetController, propertyKey: "createPetUseCase") => void {
-    throw new Error('Method not implemented.');
-  }
   static createPetUseCase = 'createPetUseCase';
+  static petRepository = 'petRepository';
+  static getPetByIdUseCase = 'getPetByIdUseCase';
 }
